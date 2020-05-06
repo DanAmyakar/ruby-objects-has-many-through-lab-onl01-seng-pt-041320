0b5a644292e7c93a2aa7d45806f234849437285c
Daniel Amyakar <dan.rockit@gmail.com>
2020-05-06 13:48:19 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,4 +16,15 @@ class Artist
     @@all
   end
   
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+  
+  def new_song(title, genre)
+    Song.new(title, self, genre)
+  end
+  
+  def genres
+    songs.collect
+  
 end
